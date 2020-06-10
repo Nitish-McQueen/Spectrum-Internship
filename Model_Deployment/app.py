fec250b415d438eb190ad568291d3444e832d7be
Nitish <54181907+Nitish-McQueen@users.noreply.github.com>
2020-06-10 23:01:13 +0530

    Add files via Upload

--- Model_Deployment/app.py
+++ Model_Deployment/app.py
@@ -3,20 +3,19 @@ from flask import Flask, render_template, request
 from sklearn.externals import joblib
 import pandas as pd
 import numpy as np
+import pickle
 
 app=Flask(__name__)
 # Load model_prediction
-mul_reg = open("Multiple_Regression_Student.pkl", "rb")
-ml_model = joblib.load(mul_reg)
+ml_model = pickle.load(open('model.pkl', 'rb'))
 
 @app.route("/")
 def home():
-    return render_template('home.html')
+    return render_template('index.html')
 
-@app.route("/predict", methods=['GET', 'POST'])
+@app.route("/predict", methods=['POST'])
 def predict():
     if request.method == 'POST':
-        try:
             absences = int(request.form['absences'])
             G1 = int(request.form['G1'])
             G2 = int(request.form['G2'])
@@ -25,9 +24,8 @@ def predict():
             pred_args_arr = pred_args_arr.reshape(1, -1)
             model_prediction=ml_model.predict(pred_args_arr)
             model_prediction = round(int(model_prediction), 2)
-        except valueError:
-            raise "Enter the values correctly..."
-    return render_template('predict.html', prediction = model_prediction)
+
+    return render_template('result.html', prediction = model_prediction)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
